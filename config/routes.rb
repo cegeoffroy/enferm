743c20ea70f3552ca295d622d3d29ded6ba9ae30
charles geoffroy <charles@dgtlpay.co>
2021-01-19 00:00:53 +0000

    account page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'accounts/index'
   devise_for :users
 
   # Pages
   root to: 'pages#home'
   get 'feed', to: 'pages#feed'
+  get 'account', to: 'accounts#index'
+
+  resources :users, only: [:show, :index]
 
 end
