99ea3379289eff07e350de071cae445dd15ffdfa
charles geoffroy <charles@dgtlpay.co>
2021-01-25 21:44:47 +0000

    account changed to dashboard and removal of activeadmin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   def home
     @user = current_user
     if user_signed_in?
-      redirect_to feed_path
+      redirect_to dashboard_path
     end
   end
   def feed
